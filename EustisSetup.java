b7a84bca0de2832688ef4761e0ffcf2373b8792d
Carolina Duarte <carolcrduarte@knights.ucf.edu>
2022-09-02 17:38:20 -0400

    Initial commit

---
+++ EustisSetup.java
@@ -0,0 +1,21 @@
+/* Carolina Duarte
+ * Dr. Steinberg 
+ * COP3503 Fall 2022
+ * Eustis Setup Assignment
+ */
+
+class EustisSetup {
+
+    //Create method
+
+    public static void main(String[] args) {
+
+        System.out.println("The current month is August.");
+        System.out.println("The current year is 2022.");
+        System.out.println("I am a student at the University of Central Florida.");
+        System.out.println("I am taking Dr. Steinberg's Computer Science 2 Course!");
+        System.out.println("I am so excited to learn more algorithms and advanced data structures!");
+
+    }
+
+}
